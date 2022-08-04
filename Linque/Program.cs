2d6e7570482b9c57d587808880c8f2abc0eb22e3
Suraj Garud <surajgarud95@gmail.com>
2022-08-04 12:55:50 +0530

    Linque

---
+++ Linque/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Linque;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to Data Table demo");
+LinqueDemo linqToDataTable = new LinqueDemo();
+linqToDataTable.AddToDataTableDemo();
+Console.ReadKey();
